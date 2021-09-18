8f0dae7a78967bf1cecc6b10f094d9dd41f35088
Ruslan Salianko <r.salianko@gmail.com>
2021-09-18 09:40:51 +0300

    function head from lodash

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 import { equal } from 'assert';
 import chai from 'chai';
-import compact from '../array/compact.js'
+import compact from '../array/compact.js';
+import head from '../array/head.js';
 
 const expect = chai.expect;
 
@@ -12,5 +13,17 @@ describe('Array', () => {
     it("compact([20, NaN, false, 0, '', null]) => [20]", () => {
       expect(compact([20, NaN, false, 0, '', null])).to.eql([20]);
     });
+  });
+
+  describe('#head(). Gets the first element of array', () => {
+    it("head([1,2,3] => 1", () => {
+      equal(head([1, 2, 3]), 1);
+    });
+    it("head([NaN,2,3] => NaN", () => {
+      equal(head([NaN, 2, 3]), NaN);
+    });
+    it("head([]) => undefine", () => {
+      equal(head([]),undefined);
+    })
   });
 });
