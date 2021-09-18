5a057c0d8604e6a336f69d4d16d0808ac3847294
Ruslan Salianko <r.salianko@gmail.com>
2021-09-18 10:20:13 +0300

    function drop from lodash

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 import { equal } from 'assert';
 import chai from 'chai';
-import compact from '../array/compact.js'
+import compact from '../array/compact.js';
+import drop from '../array/drop.js';
 
 const expect = chai.expect;
 
@@ -11,6 +12,24 @@ describe('Array', () => {
     });
     it("compact([20, NaN, false, 0, '', null]) => [20]", () => {
       expect(compact([20, NaN, false, 0, '', null])).to.eql([20]);
+    });
+  });
+
+  describe('#drop(). Creates a slice of array with n elements dropped from the beginning.', () => {
+    it("drop([1, 2, 3]) => [2, 3]", () => {
+      expect(drop([1, 2, 3])).to.eql([2, 3]);
+    });
+
+    it("drop([1, 2, 3], 2) => [3]", () => {
+      expect(drop([1, 2, 3], 2)).to.eql([3]);
+    });
+
+    it("drop([1, 2, 3], 5) => []", () => {
+      expect(drop([1, 2, 3], 5)).to.eql([]);
+    });
+
+    it("drop([1, 2, 3], 0) => [1, 2, 3]", () => {
+      expect(drop([1, 2, 3], 0)).to.eql([1, 2, 3]);
     });
   });
 });
